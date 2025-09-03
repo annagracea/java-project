f197c0049c60870b2e68468230487636ee643935
annagracea <annagraceajith0806@gmail.com>
2025-09-03 14:20:00 +0530

    edit4

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("MapMyDonor frontend loaded");
-
-// Example: Fetch donors from backend API
-function getDonors() {
-    fetch("http://localhost:8080/donors")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
-}
